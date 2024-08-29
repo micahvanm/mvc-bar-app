7079c296a39e5877436e410857999584574929d4
Micah van Meurs <n01454650@unf.edu>
2024-08-28 22:56:33 -0400

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { BarControllerComponent } from './bar-controller/bar-controller.component';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-    { path: '', component: MenuComponent }
+    { path: '', component: MainComponent },
+    { path: 'menu', component: MenuComponent }
 ];
